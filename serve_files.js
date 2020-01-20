6d4cc4fca655b2caa6d6d6c6ca4c49707add06e6
Akshat Mahajan <akshatm.bkk@gmail.com>
2020-01-20 13:15:09 -0500

    Add support ofr reading the CSV files produced by the client

--- serve_files.js
+++ serve_files.js
@@ -5,6 +5,7 @@ const app = express()
 app.use(cors())
 
 const parse_vegeta_file = filename => {
+
 	file_contents = fs.readFileSync(filename).toString()
 	newline_separated_contents = file_contents.split('\n')
 	
@@ -15,6 +16,26 @@ const parse_vegeta_file = filename => {
 	// We drop the headers and the last row (which is always an empty line).
 	//
 	return newline_separated_contents.map(parsed_row).slice(1, -1).filter(item => parseFloat(item[1]) < 0.98)
+
+}
+
+const parse_request_duration_file = filename => {
+
+	file_contents = fs.readFileSync(filename).toString()
+	newline_separated_contents = file_contents.split('\n')
+
+	// this takes a file with two comma-separated elements, and computes the numerical difference between the elements
+	parsed_row = row => row.split(',').reduce((start, end) => parseInt(start, 10) - parseInt(end, 10))
+
+	// returns an array of 2-length arrays. Each 2-length array contains response time in ms
+	// at index 0, and percentile of that response at index 1. Think of this as (x, y) pairs. 
+	// To do this correctly, I need to manually compute the percentile value, which should be easy
+	// with just indices.
+	sorted_array = newline_separated_contents.slice(1).map(parsed_row).sort((a, b) => a - b)
+
+	length = sorted_array.length()
+	return sorted_array.map((response_time, index) => [response_time, index / length])
+
 }
 
 app.get('/:headers/:rate/:concurrency/:duration/:configs/:file', function (req, res) {
@@ -22,6 +43,9 @@ app.get('/:headers/:rate/:concurrency/:duration/:configs/:file', function (req, 
 
   	if (req.params.file === "vegeta_success.plot" || req.params.file === "vegeta.bin" ) {
       		series_data = parse_vegeta_file(filename)
+      		res.json({data: series_data})
+	} else if (req.params.file === "request_duration.csv") {
+      		series_data = parse_request_duration_file(filename)
       		res.json({data: series_data})
   	} else if (req.params.file === "perf.svg") {
 		res.sendFile(filename, {root: __dirname});
